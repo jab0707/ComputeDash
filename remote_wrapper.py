2e3011173f32d0baf1f39a00be0686eb00c8aed8
Jake Bergquist <38257234+jab0707@users.noreply.github.com>
2025-01-24 16:25:06 -0700

    more fixes

--- remote_wrapper.py
+++ remote_wrapper.py
@@ -8,12 +8,12 @@ import subprocess, sys, os
 
 if __name__=="__main__":
 
-    if len(sys.argv!=3):
-        print("That didn't work")
-        return
-    interpreter=sys.argv[1]
-    args = sys.argv[2:]
-    # Pass all arguments to the main script
-    command = [interpreter, 'remote_script.py', *args]
-    print(f'Attmepting to run like so:\n{' '.join(command)} ')
-    subprocess.run(command)
+	if len(sys.argv!=3):
+		print("That didn't work")
+		sys.exit(1)
+	interpreter=sys.argv[1]
+	args = sys.argv[2:]
+	# Pass all arguments to the main script
+	command = [interpreter, 'remote_script.py', *args]
+	print(f'Attmepting to run like so:\n{' '.join(command)} ')
+	subprocess.run(command)
