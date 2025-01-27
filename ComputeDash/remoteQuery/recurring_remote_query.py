070c6e8bef7429a8f965ccb0a011404f74eed39f
Jake Bergquist <38257234+jab0707@users.noreply.github.com>
2025-01-26 18:24:44 -0700

    Update recurring_remote_query.py

--- ComputeDash/remoteQuery/recurring_remote_query.py
+++ ComputeDash/remoteQuery/recurring_remote_query.py
@@ -1,11 +1,12 @@
 import json, psutil, GPUtil, time, pathlib,sys,time,argparse
 import ComputeDash.utils.globalParams as gp
-
+import ComputeDash.utils.general_utils as gu
 def mainParser():
 	parser = argparse.ArgumentParser(add_help=False)
 	# add arguments that we may want to change from one simulation to another
 	parser.add_argument("--wait_delay", default=gp.WAIT_INTERVAL, help="")
 	parser.add_argument("--repeate_times", default=gp.REPEATE_LIMIT, help="")
+	parser.add_argument("--log_file", default='log.stats', help="")
 	return parser
 
 
@@ -28,13 +29,10 @@ if __name__ == "__main__":
 	args = parser.parse_args()
 	repeate_num = 0
 	while repeate_num < args.repeate_times:
-		if len(sys.argv) != 2:
-			print("Usage: remote_script.py <statsFile>")
-			sys.exit(1)
-		statsFile = sys.argv[1]
-		pathlib.Path(statsFile).touch()
+		pathlib.Path(args.log_file).touch()
 		current_stats = scrape_data()
-		logStats(current_stats,statsFile)
+		logStats(current_stats,args.log_file)
+		gu.infoDump('Waiting for {args.wait_delay} sec',1)
 		time.sleep(args.wait_delay)
 
 
